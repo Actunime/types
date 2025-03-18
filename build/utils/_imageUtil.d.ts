b27c67aeda308447a88fcf93f8bd42ef7118f491
DevlM <dev.maudon@gmail.com>
2025-03-18 18:55:36 +0100

    fix(v2): clean types & fix

--- build/utils/_imageUtil.d.ts
+++ build/utils/_imageUtil.d.ts
@@ -1,4 +1,3 @@
-import { ITargetPath } from "./_global";
 export declare const ImageLabelObj: {
     COVER: string;
     BANNER: string;
@@ -11,16 +10,4 @@ export declare const ImageLabelSelection: {
     label: string;
     value: "COVER" | "BANNER" | "AVATAR" | "LOGO";
 }[];
-export declare function CreateImageCDN({ IMAGE_LOCAL_HOST, IMAGE_PORT, ...value }: {
-    id: string;
-    path: ITargetPath;
-    value: string;
-    valueIsUrl: boolean;
-    IMAGE_LOCAL_HOST?: string;
-    IMAGE_PORT?: string;
-}): Promise<void>;
-export declare function DeleteImageCDN(value: {
-    id: string;
-    path: ITargetPath;
-}): Promise<void>;
 //# sourceMappingURL=_imageUtil.d.ts.map
--- src/utils/_imageUtil.ts
+++ src/utils/_imageUtil.ts
@@ -1,5 +1,3 @@
-import { ITargetPath } from "./_global";
-
 export const ImageLabelObj = {
   COVER: "Couverture",
   BANNER: "Bannière",
@@ -14,53 +12,3 @@ export const ImageLabelSelection = ImageLabelArray.map((key) => ({
   label: ImageLabelObj[key],
   value: key,
 }));
-
-export async function CreateImageCDN({
-  IMAGE_LOCAL_HOST,
-  IMAGE_PORT,
-  ...value
-}: {
-  id: string;
-  path: ITargetPath;
-  value: string;
-  valueIsUrl: boolean;
-  IMAGE_LOCAL_HOST?: string;
-  IMAGE_PORT?: string;
-}) {
-  const req = await fetch(
-    "http://" +
-    (process.env.IMAGE_LOCAL_HOST || IMAGE_LOCAL_HOST || "localhost") +
-    ":" +
-    (process.env.IMAGE_PORT || IMAGE_PORT || "3006") +
-    "/v1/create",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(value),
-    },
-  );
-
-  if (req.status !== 200)
-    throw new Error("Impossible de créer l'image");
-}
-
-export async function DeleteImageCDN(value: { id: string; path: ITargetPath }) {
-  const req = await fetch(
-    "http://" +
-    process.env.IMAGE_LOCAL_HOST +
-    ":" +
-    process.env.IMAGE_PORT +
-    "/v1/delete",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(value),
-    },
-  );
-
-  if (req.status !== 200) throw new Error("Impossible de supprimer l'image");
-}
